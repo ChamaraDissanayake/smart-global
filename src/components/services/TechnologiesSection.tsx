5e7baaa53d6b709e3b1e6ff975866603902cd4c4
Chamara Dissanayake <dchamarad91@gmail.com>
2025-03-22 10:26:54 +0400

    Technologies updated

--- src/components/services/TechnologiesSection.tsx
+++ src/components/services/TechnologiesSection.tsx
@@ -23,6 +23,7 @@ const TechnologiesSection: React.FC = () => {
         { id: "mobile", label: "Mobile App" },
         { id: "frontend", label: "Frontend" },
         { id: "backend", label: "Backend" },
+        { id: "database", label: "Database" },
         { id: "devops", label: "Cloud & Devops" },
         { id: "payment", label: "Payment" },
     ];
@@ -30,57 +31,122 @@ const TechnologiesSection: React.FC = () => {
     const technologies: Technologies = {
         mobile: [
             { name: "iOS", icon: "./icons/ios.svg" },
-            { name: "Xamarin", icon: "./icons/xamarin.svg" },
             { name: "Swift", icon: "./icons/swift.svg" },
+            { name: "SwiftUI", icon: "./icons/swift-ui.svg" },
+            { name: "Objective-C", icon: "./icons/objective-c.png" },
+
             { name: "Android", icon: "./icons/android.svg" },
             { name: "Kotlin", icon: "./icons/kotlin.svg" },
             { name: "Java", icon: "./icons/java.svg" },
+            { name: "Jetpack Compose", icon: "./icons/jetpack-compose.png" },
+
             { name: "React Native", icon: "./icons/react.svg" },
+            { name: "Expo", icon: "./icons/expo.png" },
+
             { name: "Flutter", icon: "./icons/flutter.svg" },
+
             { name: "Ionic", icon: "./icons/ionic.svg" },
             { name: "Capacitor", icon: "./icons/capacitor.svg" },
-            { name: "Cordova", icon: "./icons/cordova.svg" }
+            { name: "Cordova", icon: "./icons/cordova.svg" },
+
+            { name: "Xamarin", icon: "./icons/xamarin.svg" }
         ],
         frontend: [
+            { name: "Angular", icon: "./icons/angular.svg" },
+            { name: "React", icon: "./icons/react.svg" },
+            { name: "Vue JS", icon: "./icons/vue.svg" },
+            { name: "Svelte", icon: "./icons/svelte.png" },
+
+            { name: "JavaScript", icon: "./icons/javascript.png" },
+            { name: "TypeScript", icon: "./icons/typescript.png" },
+
             { name: "HTML", icon: "./icons/html.svg" },
             { name: "CSS", icon: "./icons/css.svg" },
+            { name: "SASS", icon: "./icons/sass.png" },
             { name: "Tailwind CSS", icon: "./icons/tailwind.svg" },
-            { name: "Angular", icon: "./icons/angular.svg" },
-            { name: "React JS", icon: "./icons/react.svg" },
+            { name: "Bootstrap", icon: "./icons/bootstrap.png" },
+
             { name: "Next JS", icon: "./icons/next.svg" },
+            { name: "Nuxt JS", icon: "./icons/nuxt.png" },
+            { name: "Astro", icon: "./icons/astro.png" },
             { name: "Vite", icon: "./icons/vite.svg" },
-            { name: "Vue JS", icon: "./icons/vue.svg" },
+
+            { name: "Redux", icon: "./icons/redux.svg" },
+            { name: "Zustand", icon: "./icons/zustand.png" }
+
         ],
         backend: [
-            { name: ".NET", icon: "./icons/dotnet.svg" },
-            { name: "Java", icon: "./icons/java.svg" },
-            { name: "Python", icon: "./icons/python.svg" },
             { name: "PHP", icon: "./icons/php.svg" },
+            { name: "Laravel", icon: "./icons/laravel.png" },
+
+            { name: "Java", icon: "./icons/java.svg" },
+            { name: "Spring Boot", icon: "./icons/springboot.png" },
+
+            { name: ".NET", icon: "./icons/dotnet.svg" },
+
+            { name: "Python", icon: "./icons/python.svg" },
+            { name: "Django", icon: "./icons/django.svg" },
+            { name: "Flask", icon: "./icons/flask.png" },
+
             { name: "Node JS", icon: "./icons/node.svg" },
-            { name: "GO", icon: "./icons/go.svg" }
+            { name: "Express.js", icon: "./icons/express.png" },
+            { name: "Fastify", icon: "./icons/fastify.png" },
+            { name: "NestJS", icon: "./icons/nestjs.png" },
+
+            { name: "GO", icon: "./icons/go.svg" },
+            { name: "Fiber", icon: "./icons/fiber.png" },
+
+            { name: "Ruby on Rails", icon: "./icons/ruby.png" }
+        ],
+        database: [
+            { name: "MySQL", icon: "./icons/my-sql.png" },
+            { name: "PostgreSQL", icon: "./icons/postgre-sql.png" },
+            { name: "MS SQL Server", icon: "./icons/ms-sql.png" },
+            { name: "Azure SQL DB", icon: "./icons/azure-sql.png" },
+            { name: "SQLite", icon: "./icons/sqlite.png" },
+
+            { name: "Oracle DB", icon: "./icons/oracle.png" },
+
+            { name: "MongoDB", icon: "./icons/mongodb.svg" },
+            { name: "CouchDB", icon: "./icons/couchdb.svg" },
+
+            { name: "Cassandra", icon: "./icons/cassandra.png" },
+
+            { name: "DynamoDB", icon: "./icons/dynamodb.svg" },
+
+            { name: "Redis", icon: "./icons/redis.svg" },
+
+            { name: "Google Firestore", icon: "./icons/firestore.png" },
+            { name: "Firebase Realtime DB", icon: "./icons/firebase.svg" },
+
+            { name: "Amazon RDS", icon: "./icons/rds.svg" }
         ],
         devops: [
             { name: "Docker", icon: "./icons/docker.svg" },
-            { name: "Kubernet", icon: "./icons/kubernet.svg" },
-            { name: "SQL Server", icon: "./icons/sql.svg" },
+            { name: "Kubernetes", icon: "./icons/kubernetes.svg" },
             { name: "Terraform", icon: "./icons/terraform.svg" },
+
             { name: "Google Cloud", icon: "./icons/google-cloud.svg" },
             { name: "AWS", icon: "./icons/aws.svg" },
             { name: "Azure", icon: "./icons/azure.svg" },
             { name: "Digital Ocean", icon: "./icons/ocean.svg" },
-            { name: "Firebase", icon: "./icons/firebase.svg" },
+
+            { name: "Firebase", icon: "./icons/firebase.svg" }
+
         ],
         payment: [
             { name: "PayPal", icon: "./icons/paypal.svg" },
             { name: "Stripe", icon: "./icons/stripe.svg" },
             { name: "Apple Pay", icon: "./icons/apple.svg" },
             { name: "Google Pay", icon: "./icons/google-pay.svg" },
-            { name: "Amazon Pay", icon: "./icons/amazon-pay.svg" },
+            { name: "Amazon Pay", icon: "./icons/amazon-pay.png" },
+
             { name: "Safepay", icon: "./icons/safe-pay.png" },
             { name: "XPay", icon: "./icons/x-pay.png" },
             { name: "Telr", icon: "./icons/telr.png" },
             { name: "CASHU", icon: "./icons/cashu.png" },
-            { name: "Crypto", icon: "./icons/crypto.svg" }
+
+            { name: "Crypto", icon: "./icons/crypto.png" }
         ]
     };
 
